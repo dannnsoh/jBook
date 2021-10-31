df974cb48112687cacceed04184c7b4b133d0137
Daniel <danielsoh123@gmail.com>
2021-10-31 21:37:08 +0800

    Complete esbuild config and in-browser bundling

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
+
+function hello() {
+	if (Math.random() !== 2) {
+		return "ok";
+	}
+}
 
 const App = () => {
 	const ref = useRef<any>();
@@ -11,7 +18,7 @@ const App = () => {
 	const startService = async () => {
 		await esbuild.initialize({
 			worker: true,
-			wasmURL: "/esbuild.wasm"
+			wasmURL: "https://unpkg.com/esbuild-wasm/esbuild.wasm"
 		});
 		ref.current = true;
 	};
@@ -28,15 +35,26 @@ const App = () => {
 				entryPoints: [ "index.js" ],
 				bundle: true,
 				write: false,
-				plugins: [ unpkgPathPlugin() ]
+				plugins: [ unpkgPathPlugin(), fetchPlugin(input) ],
+				define:
+					{
+						"process.env.NODE_ENV": '"production"',
+						global: "window"
+					}
 			});
 			setCode(result.outputFiles[0].text);
+
+			try {
+				eval(result.outputFiles[0].text);
+			} catch (err) {
+				alert(err);
+			}
 		}
 	};
 
 	return (
 		<div>
-			<textarea value={input} onChange={(event) => setInput(event.target.value)} />
+			<textarea rows={10} cols={50} value={input} onChange={(event) => setInput(event.target.value)} />
 			<div>
 				<button onClick={onClick}>Submit</button>
 			</div>
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,4 @@
 import * as esbuild from "esbuild-wasm";
-import axios from "axios";
 
 export const unpkgPathPlugin = () => {
 	return {
@@ -8,41 +7,27 @@ export const unpkgPathPlugin = () => {
 		// setup of bundling process
 		setup(build: esbuild.PluginBuild) {
 			// onResolve event listener (override esbuild's default process of figuring out where the main file is stored)
-			// can have multiple onResolve listeners for different types of files, with the regex filter changing to look for the different types of files
-			build.onResolve({ filter: /.*/ }, async (args: any) => {
-				console.log("onResolve", args);
-				if (args.path === "index.js") {
-					return { path: args.path, namespace: "a" };
-				}
+			// can have multiple onResolve functions for different types of files, with the regex filter changing to look for the different types of files
+			// return path to where the main file is stored (in this case, index.js as defined in the "entry point")
+			// namespace is an "identifier" which can be used in onLoad to apply the onLoad function to only the files with the specified namespace
 
+			// handle root entry file of index.js
+			build.onResolve({ filter: /(^index\.js$)/ }, () => {
+				return { path: "index.js", namespace: "a" };
+			});
+
+			// handle relative paths in a module
+			build.onResolve({ filter: /^\.+\// }, async (args: any) => {
+				return {
+					namespace: "a",
+					path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href
+				};
+			});
+			// handle main file of a module
+			build.onResolve({ filter: /.*/ }, async (args: any) => {
 				return {
 					namespace: "a",
 					path: `https://unpkg.com/${args.path}`
-				};
-				// else {
-				// 	// return path to where the main file is stored (in this case, index.js as defined in the "entry point")
-				// 	// namespace is an "identifier" which can be used in onLoad to apply the onLoad function to only the files with the specified namespace
-				// 	return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: "a" };
-				// }
-			});
-			// onLoad event listener (override esbuild's default file loading process)
-			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				console.log("onLoad", args);
-				if (args.path === "index.js") {
-					return {
-						loader: "jsx",
-						contents:
-							`
-                            const message = require("tiny-test-pkg");
-                            console.log(message);
-                        `
-					};
-				}
-
-				const { data }: any = await axios.get(args.path);
-				return {
-					loader: "jsx",
-					contents: data
 				};
 			});
 		}
